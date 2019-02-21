567e09e73b150dcf8dc4a1c2c8881a44dc9fb7b2
William Veillet-Berger <william.veillet-berger.1@ulaval.ca>
2019-02-21 16:54:42 -0500

    new logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" width="300px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png">
     <Home msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
